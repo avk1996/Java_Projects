9108bc8e315c344042a29cac9d41101cd15f4ff9
avk1996 <kumbhara976@gmail.com>
2026-08-13 17:29:04 +0530

    updated value name

--- unit_converter/src/main/java/com/app/unit/utility/Length.java
+++ unit_converter/src/main/java/com/app/unit/utility/Length.java
@@ -1,11 +1,10 @@
 package com.app.unit.utility;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 
 public enum Length {
     // Define each unit with its value relative to 1 Meter
-    MILIMETER(new BigDecimal("0.001")),
+    MILLIMETER(new BigDecimal("0.001")),
     CENTIMETER(new BigDecimal("0.01")),
     METER(new BigDecimal("1.0")),
     KILOMETER(new BigDecimal("1000.0")),
